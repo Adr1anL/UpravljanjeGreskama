9e1074fe2a5d70bee6fdd44cd26a9354a8915e0a
Adr1anL <146196415+Adr1anL@users.noreply.github.com>
2023-10-25 09:31:09 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trycatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int godRod;
+            bool kontrola = false;
+
+            do
+            {
+                Console.WriteLine("Upisi svoju godinu rodenja: ");
+                try
+                {
+                    godRod = Convert.ToInt16(Console.ReadLine());
+                    Console.WriteLine("Roden/a si {0}. godine.", godRod);
+                    kontrola = true;
+                }
+                catch (Exception greska)
+                {
+                    Console.WriteLine(greska.Message);
+                    kontrola = false;
+                }
+            }while (kontrola = true);
+
+            Console.ReadKey();
+        }
+    }
+}
